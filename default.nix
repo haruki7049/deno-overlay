ea79b64e3e9aefa2187b5a84edfc5fb114048eca
haruki7049 <tontonkirikiri@gmail.com>
2024-03-31 13:50:07 +0900

    chore: hoge

--- default.nix
+++ default.nix
@@ -10,9 +10,35 @@ self: super:
         hash = "sha256-i/y5T8y4RABYb2b7qAF2eP70tSPeBGtRQVL/zuY2+Ik=";
       };
 
+      nativeBuildInputs = [
+        super.autoPatchelfHook
+        super.makeWrapper
+        super.unzip
+        super.libgcc
+      ];
+
+      buildInputs = super.lib.optionals super.stdenv.isDarwin (
+        [super.libiconv super.darwin.libobjc]
+        ++ (with super.darwin.apple_sdk_11_0.frameworks; [
+          Security
+          CoreServices
+          Metals
+          MetalPerformanceShaders
+          Foundation
+          QuartzCore
+        ])
+      );
+
+      libraries = super.lib.makeLibraryPath buildInputs;
+
       installPhase = ''
-        mkdir $out/bin
+        mkdir -p $out/bin
         install -m 0755 deno $out/bin/deno
+      '';
+
+      postFixup = ''
+        wrapProgram $out/bin/deno \
+          --set LD_LIBRARY_PATH ${libraries}
       '';
     };
   };
