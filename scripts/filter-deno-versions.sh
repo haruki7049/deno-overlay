d72d0ce7703686ac14cb7cbc6cf638327a697ea8
haruki7049 <tontonkirikiri@gmail.com>
2024-06-22 21:22:36 +0900

    chore: Update filter-deno-versions.sh

--- scripts/filter-deno-versions.sh
+++ scripts/filter-deno-versions.sh
@@ -4,16 +4,14 @@
 # Filtering the version (Release's tag) from the download link of Deno, from GitHub's REST-API.
 # Usage: ./filter-deno-versions.sh <JSON>
 
-if [[ -z $1 ]]; then
-    echo "Error: No JSON file provided."
-    echo "Usage: ./filter-deno-versions.sh <JSON>"
-    echo "Process exited with code 1."
-    exit 1
+if [ -p /dev/stdin ]; then
+    download_urls=$(cat /dev/stdin | grep browser_download_url | gawk '{gsub(/ /, "\n"); print}' | uniq | grep -E 'https://.*' | tr -d '"' | sort | gawk -F/ '{ print $8 }')
+    echo $download_urls
 elif [[ -f $1 ]]; then
     download_urls=$(cat $1 | grep browser_download_url | gawk '{gsub(/ /, "\n"); print}' | uniq | grep -E 'https://.*' | tr -d '"' | sort | gawk -F/ '{ print $8 }')
     echo $download_urls
 else
     echo "Error: File not found."
-    echo "Process exited with code 2."
-    exit 2
+    echo "Process exited with code 1."
+    exit 1
 fi
