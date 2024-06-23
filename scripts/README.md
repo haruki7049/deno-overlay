f53bbaa9ba00184c8336a89c5c4c3acf97e98a67
haruki7049 <tontonkirikiri@gmail.com>
2024-06-23 09:32:07 +0900

    chore: Update scripts

--- scripts/README.md
+++ scripts/README.md
@@ -9,14 +9,6 @@
 # Get all download links
 ```
 ```bash
-./fetch-releases.sh | ./filter-download-links.sh | ./filter-x86_64-linux-links.sh
-# Get all x86_64-linux download links
-```
-```bash
-./fetch-releases.sh | ./filter-download-links.sh | ./filter-aarch64-linux-links.sh
-# Get all aarch64-linux download links
-```
-```bash
 ./fetch-releases.sh | ./filter-deno-versions.sh
 # Get all deno versions
 ```
--- scripts/filter-download-links.sh
+++ scripts/filter-download-links.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash jq
 
+# Filtering the download links from the JSON file, from GitHub's REST-API.
+# Usage: ./filter-download-links.sh <JSON>
+
 if [ -p /dev/stdin ]; then
     download_urls=$(cat /dev/stdin | jq -r '.[].assets[].browser_download_url')
     echo $download_urls
@@ -9,7 +12,7 @@ elif [[ -f $1 ]]; then
     echo $download_urls
 else
     echo "Error: File not found."
-    echo "Usage: ./filter-x86_64-linux-links.sh <JSON>"
+    echo "Usage: ./filter-download-links.sh <JSON>"
     echo "Process exited with code 1."
     exit 1
 fi
