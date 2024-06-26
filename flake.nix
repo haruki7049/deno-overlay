bd9e3aef6492c4b96c80bd65cc9314c116bdf92b
haruki7049 <tontonkirikiri@gmail.com>
2024-06-26 16:09:18 +0900

    chore: Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,22 +2,38 @@
   description = "An overlay for Deno javascript runtime";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, treefmt-nix, flake-utils }:
+  outputs = { self, nixpkgs, treefmt-nix, flake-utils, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        build-json = pkgs.rustPlatform.buildRustPackage {
-          name = "build-json";
-          src = ./scripts/build-json;
+        lib = pkgs.lib;
+        craneLib = crane.mkLib pkgs;
 
-          cargoHash = "sha256-6SFGhHHym4nDjFuG+8uU+fmmVyyX13hNz6E0JGtoORw=";
+        commonArgs = rec {
+          src = craneLib.cleanCargoSource ./scripts/build-json;
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
         };
+
+        build-json = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
       in {
         formatter = treefmtEval.config.build.wrapper;
         checks = { formatting = treefmtEval.config.build.check self; };
