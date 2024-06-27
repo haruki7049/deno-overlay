ba5d25d2cd68d7eb78323cfe17220822921af331
haruki7049 <tontonkirikiri@gmail.com>
2024-06-27 09:57:03 +0900

    chore: hoge

--- scripts/build-json/src/main.rs
+++ scripts/build-json/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::io;
+use std::process::Command;
+use std::process::Stdio;
+
 fn main() {
-    println!("Hello, world!");
+    let args = CommandLineArgs::parse();
+    let command: String = format!("cat {}", args.path);
+
+    let process = match Command::new(command).stdin(Stdio::piped()).spawn() {
+        Err(e) => panic!("failed to spawn cat command: {}", e),
+        Ok(process) => process,
+    };
+}
+
+#[derive(Debug, Parser)]
+struct CommandLineArgs {
+    path: String,
 }
