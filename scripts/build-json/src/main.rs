4d62f34ac540694da87b81c554ea7e8c57f01fae
haruki7049 <tontonkirikiri@gmail.com>
2024-06-27 13:57:15 +0900

    Edit main.rs

--- scripts/build-json/src/main.rs
+++ scripts/build-json/src/main.rs
@@ -4,22 +4,41 @@ use serde_json::Value;
 use std::io;
 use std::process::Command;
 use std::process::Stdio;
+use serde::{Serialize, Deserialize};
 
 fn main() {
     let args = CommandLineArgs::parse();
+    let source_json: Value = to_json(read_file(args.path));
 
-    let context = read_file(args.path);
+    let result: String = generate_json(source_json);
+    println!("{}", result);
 }
 
 fn to_json(string: String) -> Value {
-    serde_json::from_str(&string).unwrap()
+    serde_json::from_str(&string).expect("Failed to parse JSON")
 }
 
 fn read_file(path: String) -> String {
-    std::fs::read_to_string(&path).unwrap()
+    std::fs::read_to_string(&path).expect("Failed to read file")
+}
+
+fn generate_json(context: Value) -> String {
+    String::from("[]")
 }
 
 #[derive(Debug, Parser)]
 struct CommandLineArgs {
     path: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ReturnJson {
+    deno: Vec<DenoVersion>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DenoVersion {
+    version: String,
+    url: String,
+    nix_hash: String,
 }
