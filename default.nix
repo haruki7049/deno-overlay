5f6d7428d2ad61b44f951ab09beafc9d204be241
haruki7049 <tontonkirikiri@gmail.com>
2024-06-28 16:21:46 +0900

    feat: Change function definition

--- default.nix
+++ default.nix
@@ -1,48 +1,55 @@
 self: super: {
-  deno = {
-    "1.42.0" = super.stdenv.mkDerivation rec {
-      pname = "deno";
+  deno = let
+    pkgs = import <nixpkgs> { };
+    fetchurl = pkgs.fetchurl;
+    stdenv = pkgs.stdenv;
+
+    mkBinaryInstall = { pname ? "deno", version, url, sha256 }:
+      stdenv.mkDerivation rec {
+        inherit pname version;
+
+        src = fetchurl { inherit url sha256; };
+        sourceRoot = ".";
+
+        nativeBuildInputs =
+          [ super.autoPatchelfHook super.makeWrapper super.unzip super.libgcc ];
+
+        buildInputs = super.lib.optionals super.stdenv.isDarwin
+          ([ super.libiconv super.darwin.libobjc ]
+            ++ (with super.darwin.apple_sdk_11_0.frameworks; [
+              Security
+              CoreServices
+              Metals
+              MetalPerformanceShaders
+              Foundation
+              QuartzCore
+            ]));
+
+        libraries = super.lib.makeLibraryPath buildInputs;
+
+        installPhase = ''
+          mkdir -p $out/bin
+          install -m 0755 deno $out/bin/deno
+        '';
+
+        postFixup = ''
+          wrapProgram $out/bin/deno \
+            --set LD_LIBRARY_PATH ${libraries}
+        '';
+
+        meta = with super.lib; {
+          description = "A secure runtime for JavaScript and TypeScript";
+          homepage = "https://deno.land/";
+          mainProgram = "deno";
+          platforms = [ "x86_64-linux" ];
+          license = licenses.mit;
+        };
+      };
+  in {
+    "1.42.0" = mkBinaryInstall {
       version = "1.42.0";
-
-      src = super.fetchzip {
-        url =
-          "https://github.com/denoland/deno/releases/download/v${version}/deno-x86_64-unknown-linux-gnu.zip";
-        hash = "sha256-i/y5T8y4RABYb2b7qAF2eP70tSPeBGtRQVL/zuY2+Ik=";
-      };
-
-      nativeBuildInputs =
-        [ super.autoPatchelfHook super.makeWrapper super.unzip super.libgcc ];
-
-      buildInputs = super.lib.optionals super.stdenv.isDarwin
-        ([ super.libiconv super.darwin.libobjc ]
-          ++ (with super.darwin.apple_sdk_11_0.frameworks; [
-            Security
-            CoreServices
-            Metals
-            MetalPerformanceShaders
-            Foundation
-            QuartzCore
-          ]));
-
-      libraries = super.lib.makeLibraryPath buildInputs;
-
-      installPhase = ''
-        mkdir -p $out/bin
-        install -m 0755 deno $out/bin/deno
-      '';
-
-      postFixup = ''
-        wrapProgram $out/bin/deno \
-          --set LD_LIBRARY_PATH ${libraries}
-      '';
-
-      meta = with super.lib; {
-        description = "A secure runtime for JavaScript and TypeScript";
-        homepage = "https://deno.land/";
-        mainProgram = "deno";
-        platforms = [ "x86_64-linux" ];
-        license = licenses.mit;
-      };
+      url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
+      sha256 = "sha256-3jbacxIAeBW/V2T7eN0S94OMtdiXow55FUt0idI2Oy8=";
     };
   };
 }
