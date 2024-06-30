cbe28b7fb4f0e1635920e8a6c3fc12ec93b5de92
haruki7049 <tontonkirikiri@gmail.com>
2024-06-30 16:26:28 +0900

    chore: Delete deno but not also version and derivation

--- flake.nix
+++ flake.nix
@@ -62,21 +62,16 @@
               license = licenses.mit;
             };
           };
-        deno =
-          let
-          in
-          {
-            "1.42.0" = mkBinaryInstall {
-              version = "1.42.0";
-              url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
-              sha256 = "0brv6v98jx2b2mwhx8wpv2sqr0zp2bfpiyv4ayziay0029rxldny";
-            };
-            "1.44.4" = mkBinaryInstall {
-              version = "1.44.4";
-              url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-x86_64-unknown-linux-gnu.zip";
-              sha256 = "0mgfx70crrahpg9rj6q319k8bf3d7122zjik3ygwxl1jm89nv3y4";
-            };
-          };
+        "1.42.0" = mkBinaryInstall {
+          version = "1.42.0";
+          url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
+          sha256 = "0brv6v98jx2b2mwhx8wpv2sqr0zp2bfpiyv4ayziay0029rxldny";
+        };
+        "1.44.4" = mkBinaryInstall {
+          version = "1.44.4";
+          url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-x86_64-unknown-linux-gnu.zip";
+          sha256 = "0mgfx70crrahpg9rj6q319k8bf3d7122zjik3ygwxl1jm89nv3y4";
+        };
       in
       {
         formatter = treefmtEval.config.build.wrapper;
