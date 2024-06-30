18b33cee6dc5a752ed3b23e82dd549da548ff3a3
haruki7049 <tontonkirikiri@gmail.com>
2024-06-30 16:10:20 +0900

    chore: Init deno definition in flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,65 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        lib = pkgs.lib;
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        fetchurl = pkgs.fetchurl;
+        stdenv = pkgs.stdenv;
+        deno =
+          let
+            mkBinaryInstall = { pname ? "deno", version, url, sha256 }:
+              stdenv.mkDerivation rec {
+                inherit pname version;
+
+                src = fetchurl { inherit url sha256; };
+                sourceRoot = ".";
+
+                nativeBuildInputs = with pkgs;
+                  [ autoPatchelfHook makeWrapper unzip libgcc ];
+
+                buildInputs = lib.optionals stdenv.isDarwin
+                  ([ pkgs.libiconv pkgs.darwin.libobjc ]
+                    ++ (with pkgs.darwin.apple_sdk_11_0.frameworks; [
+                    Security
+                    CoreServices
+                    Metals
+                    MetalPerformanceShaders
+                    Foundation
+                    QuartzCore
+                  ]));
+
+                libraries = lib.makeLibraryPath buildInputs;
+
+                installPhase = ''
+                  mkdir -p $out/bin
+                  install -m 0755 deno $out/bin/deno
+                '';
+
+                postFixup = ''
+                  wrapProgram $out/bin/deno \
+                    --set LD_LIBRARY_PATH ${libraries}
+                '';
+
+                meta = with lib; {
+                  description = "A secure runtime for JavaScript and TypeScript";
+                  homepage = "https://deno.land/";
+                  mainProgram = "deno";
+                  platforms = [ "x86_64-linux" ];
+                  license = licenses.mit;
+                };
+              };
+          in {
+            "1.42.0" = mkBinaryInstall {
+              version = "1.42.0";
+              url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
+              sha256 = "0brv6v98jx2b2mwhx8wpv2sqr0zp2bfpiyv4ayziay0029rxldny";
+            };
+            "1.44.4" = mkBinaryInstall {
+              version = "1.44.4";
+              url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-x86_64-unknown-linux-gnu.zip";
+              sha256 = "0mgfx70crrahpg9rj6q319k8bf3d7122zjik3ygwxl1jm89nv3y4";
+            };
+          };
       in
       {
         formatter = treefmtEval.config.build.wrapper;
