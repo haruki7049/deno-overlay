4a9f1eb9a42e942b6c63331e7659466947cda1a1
haruki7049 <tontonkirikiri@gmail.com>
2024-06-30 12:28:11 +0900

    chore: Merge main branch

--- default.nix
+++ default.nix
@@ -53,12 +53,12 @@ self: super: {
       "1.42.0" = mkBinaryInstall {
         version = "1.42.0";
         url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
-        sha256 = "sha256-3jbacxIAeBW/V2T7eN0S94OMtdiXow55FUt0idI2Oy8=";
+        sha256 = "0brv6v98jx2b2mwhx8wpv2sqr0zp2bfpiyv4ayziay0029rxldny";
       };
       "1.44.4" = mkBinaryInstall {
         version = "1.44.4";
         url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-x86_64-unknown-linux-gnu.zip";
-        sha256 = "sha256-xI9tE6oy0M6fHzPKL0Q4bbiFZgoDG5nTu1DlzMDp7lU=";
+        sha256 = "0mgfx70crrahpg9rj6q319k8bf3d7122zjik3ygwxl1jm89nv3y4";
       };
     };
 }
