8e06e7d0aa02a99d423366904e209a4285d9c214
haruki7049 <tontonkirikiri@gmail.com>
2024-07-14 20:47:30 +0900

    feat: Use map and listToAttrs functions

--- default.nix
+++ default.nix
@@ -49,16 +49,5 @@ self: super: {
           };
         };
     in
-    {
-      "1.42.0" = mkBinaryInstall {
-        version = "1.42.0";
-        url = "https://github.com/denoland/deno/releases/download/v1.42.0/deno-x86_64-unknown-linux-gnu.zip";
-        sha256 = "0brv6v98jx2b2mwhx8wpv2sqr0zp2bfpiyv4ayziay0029rxldny";
-      };
-      "1.44.4" = mkBinaryInstall {
-        version = "1.44.4";
-        url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-x86_64-unknown-linux-gnu.zip";
-        sha256 = "0mgfx70crrahpg9rj6q319k8bf3d7122zjik3ygwxl1jm89nv3y4";
-      };
-    };
+    builtins.listToAttrs (map (v: {name = v.version; value = mkBinaryInstall { version = v.version; url = v.url; sha256 = v.sha256; };}) sources.deno);
 }
