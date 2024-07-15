2976865d6e8ea5286e5029ed62d8cb64f733f0cd
haruki7049 <tontonkirikiri@gmail.com>
2024-07-15 10:44:38 +0900

    fix: Update fetch-releases.py

--- fetch-releases.py
+++ fetch-releases.py
@@ -125,17 +125,15 @@ def gen_releases_list(versions: list, x86_64_linux_urls: list, aarch64_linux_url
 
     for url in x86_64_linux_urls:
         for version in versions:
-            version = version.replace("v", "")
-
             if is_correct_version_url(version, url):
                 print("Generating nix hash for", url)
                 sha256 = gen_nix_hash(url)
-                result.append({"version": version, "url": url, "arch": "x86_64-linux", "sha256": sha256})
+                result.append({"version": version.replace("v", ""), "url": url, "arch": "x86_64-linux", "sha256": sha256})
 
             elif is_correct_rc_version_url(version, url):
                 print("Generating nix hash for", url)
                 sha256 = gen_nix_hash(url)
-                result.append({"version": version, "url": url, "arch": "x86_64-linux", "sha256": sha256})
+                result.append({"version": version.replace("v", ""), "url": url, "arch": "x86_64-linux", "sha256": sha256})
 
     return result
 
