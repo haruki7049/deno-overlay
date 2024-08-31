9dbf332acfa6a544fb72e7c041b44c95d70d4c1b
haruki7049 <tontonkirikiri@gmail.com>
2024-08-31 16:59:28 +0900

    test(treefmt-nix): Add actionlint

--- treefmt.nix
+++ treefmt.nix
@@ -2,5 +2,6 @@
   projectRootFile = "treefmt.nix";
   programs.nixpkgs-fmt.enable = true;
   programs.ruff.enable = true;
+  programs.actionlint.enable = true;
   programs.shellcheck.enable = true;
 }
