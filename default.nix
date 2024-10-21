fa4e4758e168132baa0e6fe7e7e3ee3dcf30f5d9
haruki7049 <tontonkirikiri@gmail.com>
2024-10-21 22:11:19 +0900

    chore(dir-struct): Add nix directory

--- default.nix
+++ default.nix
@@ -2,51 +2,8 @@ self: super: {
   deno =
     let
       lib = super.lib;
-      fetchurl = super.fetchurl;
-      stdenv = super.stdenv;
       sources = lib.importJSON ./sources.json;
-
-      mkBinaryInstall = { pname ? "deno", version, url, sha256 }:
-        stdenv.mkDerivation rec {
-          inherit pname version;
-
-          src = fetchurl { inherit url sha256; };
-          sourceRoot = ".";
-
-          nativeBuildInputs =
-            [ super.autoPatchelfHook super.makeWrapper super.unzip super.libgcc ];
-
-          buildInputs = super.lib.optionals super.stdenv.isDarwin
-            ([ super.libiconv super.darwin.libobjc ]
-              ++ (with super.darwin.apple_sdk_11_0.frameworks; [
-              Security
-              CoreServices
-              Metals
-              MetalPerformanceShaders
-              Foundation
-              QuartzCore
-            ]));
-
-          libraries = super.lib.makeLibraryPath buildInputs;
-
-          installPhase = ''
-            mkdir -p $out/bin
-            install -m 0755 deno $out/bin/deno
-          '';
-
-          postFixup = ''
-            wrapProgram $out/bin/deno \
-              --set LD_LIBRARY_PATH ${libraries}
-          '';
-
-          meta = with super.lib; {
-            description = "A secure runtime for JavaScript and TypeScript";
-            homepage = "https://deno.land/";
-            mainProgram = "deno";
-            platforms = [ "x86_64-linux" ];
-            license = licenses.mit;
-          };
-        };
+      mkBinaryInstall = super.callPackage ./nix/mkBinaryInstall.nix { };
     in
     builtins.listToAttrs (map (v: { name = v.version; value = mkBinaryInstall { version = v.version; url = v.url; sha256 = v.sha256; }; }) sources.deno);
 }
---
+++ nix/mkBinaryInstall.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  stdenv,
+  darwin,
+  fetchurl,
+  autoPatchelfHook,
+  makeWrapper,
+  unzip,
+  libgcc,
+  libiconv,
+}:
+
+{
+  pname ? "deno",
+  version,
+  url,
+  sha256,
+}:
+
+stdenv.mkDerivation rec {
+  inherit pname version;
+
+  src = fetchurl { inherit url sha256; };
+  sourceRoot = ".";
+
+  nativeBuildInputs =
+    [ autoPatchelfHook makeWrapper unzip libgcc ];
+
+  buildInputs = lib.optionals stdenv.isDarwin
+    ([ libiconv darwin.libobjc ]
+      ++ (with darwin.apple_sdk_11_0.frameworks; [
+      Security
+      CoreServices
+      Metals
+      MetalPerformanceShaders
+      Foundation
+      QuartzCore
+    ]));
+
+  libraries = lib.makeLibraryPath buildInputs;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -m 0755 deno $out/bin/deno
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/deno \
+      --set LD_LIBRARY_PATH ${libraries}
+  '';
+
+  meta = {
+    description = "A secure runtime for JavaScript and TypeScript";
+    homepage = "https://deno.land/";
+    mainProgram = "deno";
+    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.mit;
+  };
+}
