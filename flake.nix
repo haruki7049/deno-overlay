56061729a0d9bc94e8d0c018a4091c52647ad959
haruki7049 <tontonkirikiri@gmail.com>
2024-10-21 21:39:45 +0900

    chore(flake.nix): Change flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,24 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, treefmt-nix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      in
-      {
-        formatter = treefmtEval.config.build.wrapper;
-        checks = {
-          formatting = treefmtEval.config.build.check self;
-        };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = { pkgs, ... }: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+          programs.ruff.enable = true;
+          programs.actionlint.enable = true;
+          programs.shellcheck.enable = true;
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nil
@@ -28,5 +32,6 @@
             python311
           ];
         };
-      });
+      };
+    };
 }
--- treefmt.nix
+++
@@ -1,7 +0,0 @@
-{
-  projectRootFile = "treefmt.nix";
-  programs.nixpkgs-fmt.enable = true;
-  programs.ruff.enable = true;
-  programs.actionlint.enable = true;
-  programs.shellcheck.enable = true;
-}
