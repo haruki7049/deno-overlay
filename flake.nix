2fc8e5230ce9ceb6ccad45e3084b32aa5755e72d
haruki7049 <tontonkirikiri@gmail.com>
2024-10-21 22:39:00 +0900

    test(flake.nix): Add checks

--- flake.nix
+++ flake.nix
@@ -16,6 +16,9 @@
 
   outputs =
     inputs:
+    let
+      deno-overlay = import ./.;
+    in
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
 
@@ -23,9 +26,32 @@
         inputs.treefmt-nix.flakeModule
       ];
 
+      flake.overlays = {
+        inherit deno-overlay;
+      };
+
       perSystem =
-        { pkgs, ... }:
         {
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              deno-overlay
+            ];
+          };
+
+          checks = {
+            v0-36-0 = pkgs.callPackage ./example/v0.36.0 { };
+            v1-42-0 = pkgs.callPackage ./example/v1.42.0 { };
+            v1-44-4 = pkgs.callPackage ./example/v1.44.4 { };
+            v2-0-0 = pkgs.callPackage ./example/v2.0.0 { };
+          };
+
           treefmt = {
             projectRootFile = "flake.nix";
             programs.nixfmt.enable = true;
